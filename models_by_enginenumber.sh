6dc0ed87e9538c8d5bc96b8a43a1cec4ab57877d
xalmond <xalmond@gmail.com>
2015-11-15 10:13:10 +0100

    Update comments

--- models_by_enginenumber.sh
+++ models_by_enginenumber.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Parameters - $1 datafile $2 number of engines
-# Output - Number of models by number of engines
+# Parameters: $1 datafile $2 number of engines
+# Output: Number of models by number of engines
 
 csvgrep -d '^' -c nb_engines -m $2 $1 | tail -n +2 | wc -l
