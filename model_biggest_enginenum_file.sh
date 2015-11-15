d8348c8bcf66139cf53f21c1a2a4cbe986e6defb
xalmond <xalmond@gmail.com>
2015-11-15 10:21:24 +0100

    Update comments and rename file

--- model_biggest_enginenum_file.sh
+++ model_biggest_enginenum_file.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-# Esto es un comentario
+# Parameters: $1 Datafile
+# Output: Model with biggest number of engines
+
 csvsort  -r -d '^' -c nb_engines $1 | head -2 | csvcut -c model | tail -1
