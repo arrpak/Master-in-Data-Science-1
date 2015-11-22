0681cf1d0f6e2c53ded765f864099f8003d1636f
xalmond <xalmond@gmail.com>
2015-11-22 09:50:47 +0100

    Delete model_biggest_enginenum_file_manufacturer.sh

--- model_biggest_enginenum_file_manufacturer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Parameters: $1 Datafile $2 Manufacturer
-# Output: Model with biggest number of engines of manufacturer
-
-csvgrep -d '^' -c manufacturer -m $2 $1 | csvsort  -r -c nb_engines | csvcut -c model | head -2 | tail -1
