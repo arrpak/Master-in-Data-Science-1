cb9d70da12ca96038963b891a539a1aad92994f9
Javier Almendro <xalmond@gmail.com>
2015-11-25 08:10:14 +0100

    more sql commands

--- sql/mylibrary.sql
+++ sql/mylibrary.sql
@@ -18,7 +18,6 @@ INSERT INTO  myLibrary VALUES ('MindSwap','Robert Sheckley',NULL,'Wiley',224,'20
 INSERT INTO  myLibrary VALUES ('John Livigsnton Seagull','Richard Bach',NULL,'McMillan',100,CURRENT_DATE,'978-0470229001','English');
 
 SELECT * FROM myLibrary;
-SELECT TOP 2 * FROM myLibrary;
 SELECT * FROM myLibrary LIMIT 2;
 
 UPDATE mylibrary SET pages=500,title='SQL Bible, 2nd Edition' WHERE title='SQL Bible';
@@ -35,4 +34,36 @@ SELECT * FROM myLibrary;
 INSERT INTO  myLibrary VALUES ('John Livigsnton Seagull','Richard Bach',NULL,'McMillan',100,CURRENT_DATE,'978-0470229001','English',18.60);
 SELECT * FROM myLibrary WHERE author2 IS NULL;
 SELECT * FROM myLibrary ORDER BY publisher DESC, author DESC;
+
 SELECT * INTO cheapmyLibary FROM myLibrary WHERE price <25.00 ORDER BY price;
+SELECT * FROM cheapmyLibary;
+DROP TABLE cheapmyLibary;
+
+SELECT * FROM myLibrary WHERE price BETWEEN 20 AND 26;
+SELECT * FROM myLibrary WHERE pages IN (100,200,300,400,500);
+SELECT * FROM myLibrary WHERE author LIKE 'R%';
+
+SELECT publisher,SUM(pages) AS tot_pages FROM myLibrary GROUP BY publisher ORDER BY tot_pages DESC;
+-- Wrong SELECT publisher FROM myLibrary WHERE SUM(pages)>150;
+SELECT publisher,SUM(pages) AS tot_pages FROM myLibrary GROUP BY publisher HAVING SUM(pages)>150;
+
+--Subqueries
+SELECT * FROM myLibrary WHERE pages>(SELECT AVG(pages) FROM myLibrary);
+SELECT * FROM myLibrary WHERE pages IN (SELECT pages FROM myLibrary WHERE author LIKE 'R%');
+SELECT * FROM myLibrary WHERE pages = ANY (SELECT pages FROM myLibrary WHERE author LIKE 'R%');
+
+DROP TABLE IF EXISTS tmpLibrary;
+CREATE TABLE tmplibrary
+(
+    title           VARCHAR(100),
+    author          VARCHAR(100),
+    author2         VARCHAR(100),
+    publisher       VARCHAR(100),
+    pages           INT,
+    publish_date    DATE,
+    isbn            VARCHAR(100) NOT NULL,
+    book_language   VARCHAR(100),
+    price           REAL
+);
+INSERT INTO tmplibrary SELECT * FROM myLibrary WHERE pages<(SELECT AVG(pages) FROM myLibrary);
+SELECT * FROM tmplibrary;
