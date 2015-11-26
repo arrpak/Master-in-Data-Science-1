50f76e59b6276418d5854b51da3166ae07088796
Javier Almendro <xalmond@gmail.com>
2015-11-26 08:17:31 +0100

    learning subqueries

--- sql/complexqueries.sql
+++ sql/complexqueries.sql
@@ -1,50 +1,56 @@
-/* --- Top Ten de paises
-SELECT country_name, SUM(population) AS npop 
-FROM optd_por_public 
-WHERE population IS NOT NULL 
+-- Top Ten countries with more population
+
+SELECT country_name, SUM(population) AS tpop FROM optd_por_public
+WHERE population IS NOT NULL
 GROUP BY country_name 
-ORDER BY npop DESC LIMIT 10;
----Pop ten de aeropuertos per capita
-SELECT country_name, count(*) AS nairports
-FROM optd_por_public 
-WHERE location_type='A'OR location_type='CA' 
+ORDER BY tpop DESC LIMIT 10;
+
+-- Number of airports per country
+
+SELECT country_name, count(*) AS nair FROM optd_por_public
+WHERE location_type='A' OR location_type='CA'
 GROUP BY country_name 
-ORDER BY nairports LIMIT 10
-*/
+ORDER BY nair DESC LIMIT 10;
 
---- Método de "unimos ambas con inner join, solo que con una tabla" usamos CAST y float para problemas con decimales y enteros.
+-- Using both subqueries
 
-SELECT t1.country_name, npop, nairports, CAST(nairports AS float/npop)*100 AS percapita
-FROM
+SELECT t1.country_name, tpop/t2.nair AS aperc FROM
 
-(SELECT country_name, SUM(population) AS npop 
-FROM optd_por_public 
-WHERE population IS NOT NULL 
-GROUP BY country_name) AS t1,
+    (SELECT country_name, SUM(population) AS tpop FROM optd_por_public
+    WHERE population IS NOT NULL
+    GROUP BY country_name 
+    ORDER BY tpop) as t1,
 
-(SELECT country_name, count(*) AS nairports
-FROM optd_por_public 
-WHERE location_type='A'OR location_type='CA' 
-GROUP BY country_name) AS t2
+    (SELECT country_name, count(*) AS nair FROM optd_por_public
+    WHERE location_type='A' OR location_type='CA'
+    GROUP BY country_name 
+    ORDER BY nair) as t2
 
-WHERE (t1.country_name = t2.country_name) AND npop > 0
-ORDER BY percapita DESC LIMIT 10;
+WHERE (t1.country_name=t2.country_name) AND t1.tpop >0
+ORDER BY aperc;
 
+-- Creating views
 
+DROP VIEW t1;
+CREATE VIEW t1 AS 
+(
+    SELECT country_name, SUM(population) AS tpop FROM optd_por_public
+    WHERE population IS NOT NULL
+    GROUP BY country_name 
+    ORDER BY tpop
+);
 
---- Lo mismo anterior pero mediante el uso de vistas. Creando ambas vistas primero.
+DROP VIEW t2;
+CREATE VIEW t2 AS 
+(
+    SELECT country_name, count(*) AS nair FROM optd_por_public
+    WHERE location_type='A' OR location_type='CA'
+    GROUP BY country_name 
+    ORDER BY nair
+);
 
-SELECT populations2.country_name, npop, nairports, CAST(nairports AS float/npop)*100 AS percapita
-FROM populations2, airports2
-WHERE (populations2.country_name = airports2.country_name) AND npop > 0
-ORDER BY percapita DESC LIMIT 10;
+-- Both using views
 
---- Sintaxis Inner Join
-
-SELECT populations2.country_name, npop, nairports, CAST(nairports AS float/npop)*100 AS percapita
-FROM populations2
-INNER JOIN airports2
-ON (populations2.country_name = airports2.country_name)
-ORDER BY percapita DESC LIMIT 10;
-
-
+SELECT t1.country_name, (t1.tpop/t2.nair) AS aperc FROM t1,t2
+WHERE t1.country_name=t2.country_name AND t1.tpop>0
+ORDER BY aperc LIMIT 10;
