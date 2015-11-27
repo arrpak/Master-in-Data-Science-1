102660fb1997cdf202adc3cc72e1243f2706e3ee
Javier Almendro <xalmond@gmail.com>
2015-11-27 21:18:40 +0100

    Updating Comments

--- sh/top_manufacturers_file.sh
+++ sh/top_manufacturers_file.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Parameters: $1 Number of top manufacturers $2  Datafile
-# Output: Top manufacturers
+# Output: Number of planes and Top manufacturers 
 
 csvcut -d '^' -c manufacturer $2 | tail -n +2 | sort | uniq -c |tail -n +2 | sort -r -n | head -$1
