f6c341169a715542f78304eb95c6c475d6eb1f51
Javier Almendro <xalmond@gmail.com>
2016-01-09 17:54:59 +0100

    prueba

--- sh/columns_counter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Parameters: $1  ziped Datafile 
-# Output: Number of columns of Datafile
-
-zless $1 | head -1 | tr "," "\n" | wc -l
--- sh/modelnum_file_enginenum.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Parameters: $1 Datafile $2 Number of Engines
-# Output: Number of aircrafs with $2 number of engines of $1 Datafile
-
-csvgrep -d '^' -c nb_engines -m $2 $1 | tail -n +2 | wc -l
