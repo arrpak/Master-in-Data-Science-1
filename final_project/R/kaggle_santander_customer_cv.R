fcbe4fabb2bbaa6d503b731ff180a18c8967b9fa
xalmond <xalmond@gmail.com>
2016-05-11 07:14:15 +0200

    Adding Cross Validation
    Adding Cross Validation - khold

--- final_project/R/kaggle_santander_customer_cv.R
+++ final_project/R/kaggle_santander_customer_cv.R
@@ -10,15 +10,12 @@ df_train$TARGET <- NULL
 X <- as.matrix(df_train)
 y <- df_target
 
-nrounds <- 50
-xgb_param <- expand.grid(eta = c(0.01,0.015,0.02),
-                      max.depth = 5,
-                      colsample_bytree = c(0.25,0.5,0.75),
-                      subsample = c(0.25,0.5,0.75),
-                      gamma = 0,
-                      min_child_weight = 1)
+nrounds <- 360
+xgb_param <- expand.grid(eta = c(0.01,0.02,0.03),
+                         colsample_bytree = c(0.25,0.5,0.75),
+                         subsample = c(0.25,0.5,0.75))
 
-result_cv <- data.frame(id=1:50)
+result_cv <- data.frame()
 
 for (n in 1:nrow(xgb_param)){
   set.seed(1234)
@@ -26,21 +23,47 @@ for (n in 1:nrow(xgb_param)){
                     label = y,
                     nrounds = nrounds,
                     objective = 'binary:logistic',
-                    eta = xgb_param[n,'eta'],
-                    max.depth = xgb_param[n,'max.depth'],
-                    gamma = xgb_param[n,'gamma'],
-                    min_child_weight = xgb_param[n,'min_child_weight'],
-                    colsample_bytree = xgb_param[n,'colsample_bytree'],
-                    subsample = xgb_param[n,'subsample'],
+                    max.depth = 5,
+                    gamma = 0,
+                    min_child_weight = 1,
                     missing = NA,
                     nfold = 5,
                     metrics = list('auc','logloss','error'),
                     early.stop.round = 150,
                     maximize=F,
-                    verbose=0)
-  result_cv[[paste0(n)]] <- xgb_row$test.auc.mean
+                    verbose=0,
+                    eta = xgb_param[n,'eta'],
+                    colsample_bytree = xgb_param[n,'colsample_bytree'],
+                    subsample = xgb_param[n,'subsample'])
+  
+  result_cv <- rbind(result_cv,t(data.frame(xgb_row$test.auc.mean)))
+  print(n)
 }
 
-# 451 for i=1
+result_cv <- cbind(xgb_param,result_cv)
 
-write.csv(result_cv, '../data/result_cv.csv')
+write.csv(result_cv, '../data/result_cv360.csv')
+
+which_round_max_auc <- which.max(t(apply(result_cv[,4:363],2,max)))
+which_param_max_auc <- which.max(result_cv[,which_round_max_auc])
+
+result_cv[which_param_max_auc,c(1,2,3,3+which_round_max_auc)]
+
+nrounds <- 720
+xgb_row <- xgb.cv(data = X,
+                  label = y,
+                  nrounds = nrounds,
+                  objective = 'binary:logistic',
+                  max.depth = 5,
+                  gamma = 0,
+                  min_child_weight = 1,
+                  missing = NA,
+                  nfold = 5,
+                  metrics = list('auc','logloss','error'),
+                  early.stop.round = 150,
+                  maximize=F,
+                  verbose=0,
+                  eta = 0.03,
+                  colsample_bytree = 0.5,
+                  subsample = xgb_param = 0.75)
+
